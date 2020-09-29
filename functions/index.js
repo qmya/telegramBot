d1372b27701d4a9f6e8f708e1ad9145c59c03c4a
qmya <qandeelgul90@gmail.com>
2020-09-29 17:18:32 -0400

    uncommit the function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions');
+const { Telegraf } = require('telegraf');
 
+const bot = new Telegraf(1333824857, AAGs9KNqQMkZh1niOaRJ_UY - MVME599D3Cs)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
 });
